9bdadb69ecdfc51a5f38e8f85537767622ef60da
Pedro Franca <pedrom.franca@hotmail.com>
2023-07-06 21:59:26 -0300

    Finalizando exercicio dos asteriscos

---
+++ fundamentos/secao3/indroducao-java/dia2 -array-repeticao/desafio-asterisco.js
@@ -0,0 +1,11 @@
+n = 5;
+
+for (let line = 0; line < n; line += 1) {
+    let asteriscos = "";
+
+    for (let column = 0; column < n; column += 1){
+        asteriscos += "*";
+    }
+    console.log(asteriscos);
+}
+
