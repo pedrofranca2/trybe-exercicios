3bfe1b4f500197f7744ed7b49f899ba8451c4f67
Pedro Franca <pedrom.franca@hotmail.com>
2023-08-06 20:08:41 -0300

    finalizando exercicio da segunda etapa

---
+++ fundamentos-2-periodo/secao5/dia-1-html-e-css-forms/script.js
@@ -0,0 +1,19 @@
+const inputText = document.querySelector("#input-text");
+const inputCheckbox = document.querySelector("#input-checkbox");
+const hrefLink = document.querySelector("#href");
+
+hrefLink.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+inputCheckbox.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+
+  inputText.addEventListener('keypress', (event) => {
+    const inputText2 = event.key;
+    
+    if(inputText2 !== 'a'){
+    event.preventDefault();
+    }
+  });
