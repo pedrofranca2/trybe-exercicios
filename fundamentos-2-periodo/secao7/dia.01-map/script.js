5a4b13011dc6b6f14baefbd44fa095466069510e
Pedro Franca <pedrom.franca@hotmail.com>
2023-08-23 16:32:48 -0300

    feat: conclusão ex4

--- fundamentos-2-periodo/secao7/dia.01-map/script.js
+++ fundamentos-2-periodo/secao7/dia.01-map/script.js
@@ -1,3 +1,4 @@
+const data = require('./data');
 // Dado um array de temperaturas em graus Celsius, crie uma função que retorne um novo array com as temperaturas convertidas para Fahrenheit. A fórmula de conversão é: Fahrenheit = (Celsius * 9/5) + 32
 
 // Temperaturas em graus Celsius
@@ -24,57 +25,15 @@ const raizQuadrada = () => {
 
 // console.log(raizQuadrada());
 
-{
-    books: [
-      {
-        id: 1,
-        name: 'As Crônicas de Gelo e Fogo',
-        genre: 'Fantasia',
-        author: {
-          name: 'George R. R. Martin',
-          birthYear: 1948,
-        },
-        releaseYear: 1991,
-      },
-      {
-        id: 2,
-        name: 'O Senhor dos Anéis',
-        genre: 'Fantasia',
-        author: {
-          name: 'J. R. R. Tolkien',
-          birthYear: 1892,
-        },
-        releaseYear: 1954,
-      },
-      {
-        id: 3,
-        name: 'Fundação',
-        genre: 'Ficção Científica',
-        author: {
-          name: 'Isaac Asimov',
-          birthYear: 1920,
-        },
-        releaseYear: 1951,
-      },
-      {
-        id: 4,
-        name: 'Duna',
-        genre: 'Ficção Científica',
-        author: {
-          name: 'Frank Herbert',
-          birthYear: 1920,
-        },
-        releaseYear: 1965,
-      },
-      {
-        id: 5,
-        name: 'A Coisa',
-        genre: 'Terror',
-        author: {
-          name: 'Stephen King',
-          birthYear: 1947,
-        },
-        releaseYear: 1986,
-      },
-    ],
-  };
+
+//   Implemente a função formatedBookNames que deve retornar um array de strings no formato: Nome do livro - Gênero - Nome da pessoa autora.
+
+const formatedBookNames = () => {
+    const eachBook = data.books.map((book) => {
+        return `${book.name} - ${book.genre} - ${book.author.name}`;    
+    })
+
+    return eachBook;
+}
+
+// console.log(formatedBookNames());
