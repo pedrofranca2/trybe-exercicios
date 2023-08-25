a2cc577f6978c1b8e87fe7f04d78b380797e286f
Pedro Franca <pedrom.franca@hotmail.com>
2023-08-25 19:09:13 -0300

    feat: exercicios foreach primeira parte

--- fundamentos-2-periodo/secao7/metodos-de-array/dia.3/scrip.js
+++ fundamentos-2-periodo/secao7/metodos-de-array/dia.3/scrip.js
@@ -12,27 +12,49 @@
 // //   console.log(findSong);
 
 
-  const contacts = [
-    { name: 'João', phone: '123456789' },
-    { name: 'Maria', phone: '987654321' },
-    { name: 'Pedro', phone: '456123789' },
-    { name: 'Ana', phone: '789456123' },
-    { name: 'Carlos', phone: '321654987' },
-    { name: 'Mariana', phone: '654123987' },
-    { name: 'Lucas', phone: '987321654' },
-    { name: 'Laura', phone: '123789456' },
-    { name: 'Tiago', phone: '789123456' },
-    { name: 'Beatriz', phone: '456789123' }
-  ];
+//   const contacts = [
+//     { name: 'João', phone: '123456789' },
+//     { name: 'Maria', phone: '987654321' },
+//     { name: 'Pedro', phone: '456123789' },
+//     { name: 'Ana', phone: '789456123' },
+//     { name: 'Carlos', phone: '321654987' },
+//     { name: 'Mariana', phone: '654123987' },
+//     { name: 'Lucas', phone: '987321654' },
+//     { name: 'Laura', phone: '123789456' },
+//     { name: 'Tiago', phone: '789123456' },
+//     { name: 'Beatriz', phone: '456789123' }
+//   ];
   
-  const searchContact = (contacts, name) => {
-    const findContacts = contacts.find((contact) => contact.name === name);
+//   const searchContact = (contacts, name) => {
+//     const findContacts = contacts.find((contact) => contact.name === name);
 
-            if(findContacts) {
-                return {...findContacts}
-            }
-            return "Contato não encontrado"
-    }
+//             if(findContacts) {
+//                 return {...findContacts}
+//             }
+//             return "Contato não encontrado"
+//     }
 
 
-  console.log(searchContact(contacts, 'Pedro'));
+//   console.log(searchContact(contacts, 'Pedro'));
+
+
+const cartoons = [
+    { title: 'Tom & Jerry', ageRating: 'Livre' },
+    { title: 'Scooby-Doo', ageRating: 'Livre' },
+    { title: 'Looney Tunes', ageRating: 'Livre' },
+    { title: 'The Simpsons', ageRating: 12 },
+    { title: 'Avatar: The Last Airbender', ageRating: 'Livre' },
+    { title: 'Pokémon', ageRating: 'Livre' },
+    { title: 'Dragon Ball Z', ageRating: 12 },
+    { title: 'Adventure Time', ageRating: 'Livre' },
+    { title: 'Gravity Falls', ageRating: 'Livre' },
+    { title: 'Teen Titans Go!', ageRating: 'Livre' }
+  ];
+
+  const addGender = () => { 
+    cartoons.forEach((cartoon) => {
+        cartoon.gender = 'Animação'
+    })
+    return cartoons;
+  }
+  console.log(addGender());
