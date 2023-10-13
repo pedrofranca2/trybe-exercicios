ac4020321ce18614d9708f04bfb8f6757bafe484
Pedro Franca <pedrom.franca@hotmail.com>
2023-10-13 13:06:47 -0300

    feat: finalizando api heroes

--- modulo-front-end/dia2/api-super-heroes/API Heroes/src/main.js
+++ modulo-front-end/dia2/api-super-heroes/API Heroes/src/main.js
@@ -1,3 +1,29 @@
+import Swal from "sweetalert2";
+
 const API_BASE = "https://akabab.github.io/superhero-api/api";
 
-export function getHeroes
+const btn = document.querySelector("#button");
+const imgHeroe = document.querySelector("#img");
+const nameText = document.querySelector("#name");
+
+const MAX_HEROES = 1000;
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const id = Math.floor(Math.random() * MAX_HEROES);
+
+fetch(`${API_BASE}/id/${id}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+        imgHeroe.src = data.images.md;
+        nameText.innerHTML = data.name;
+    })
+    .catch((error) => Swal.fire({
+        icon: 'error',
+        title: 'Hero not found, try again',
+        text: error.message,
+        onfirmButtonText: 'Cool',
+      }));
+});
+
+
