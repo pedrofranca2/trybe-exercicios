1d1dd5be2fcb020d6e7ecd12f7055f5185e7ce48
Pedro Franca <pedrom.franca@hotmail.com>
2023-11-23 12:06:30 -0300

    atualizando e fazendo testes

--- modulo-front-end/dia1/validador_de_campos/src/main.js
+++ modulo-front-end/dia1/validador_de_campos/src/main.js
@@ -1,20 +1,19 @@
-import validator from "validator";
+import validator from 'validator';
 
 const inputValue = document.querySelector('#value');
 const textFooter = document.querySelector('#footer');
 const btnValidator = document.querySelector('#btnValidator');
 const optionSelected = document.querySelector('#typeValue');
 
-
 btnValidator.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const validatorValues = {
+  const validatorValues = {
     email: validator.isEmail(inputValue.value),
     cpf: validator.isTaxID(inputValue.value, 'pt-BR'),
     url: validator.isURL(inputValue.value),
     date: validator.isDate(inputValue.value),
-    };
+  };
 
-textFooter.innerHTML = `A validação retornou ${validatorValues[optionSelected.value]}`
-})
+  textFooter.innerHTML = `A validação retornou ${validatorValues[optionSelected.value]}`;
+});
